0503e689876ba665f3fb808de836155b62533123
cza <840270646@QQ.COM>
2024-09-05 23:34:44 +0800

    1

--- assets/Scene.ls
+++ assets/Scene.ls
@@ -1,6 +1,7 @@
 {
   "_$ver": 1,
   "_$id": "lx8mwule",
+  "_$runtime": "res://cbc92839-4208-4fd6-baa2-e450d1c56b2f",
   "_$type": "Scene",
   "left": 0,
   "right": 0,
--- src/Loading.ts
+++ src/Loading.ts
@@ -4,6 +4,17 @@ import { LoadingBase } from "./Loading.generated";
 @regClass()
 export class Loading extends LoadingBase {
     onAwake(): void {
-        
+        Laya.loader.load(["resources/progress.png","resources/progress$bar.png","resources/bg_day.png"]).then((res)=>{
+            const progress=new Laya.ProgressBar();
+            progress.width=300;
+            progress.height=40;
+            progress.x=200;
+            progress.y=100;
+            progress.anchorX=0.5;
+            progress.anchorY=0.5;
+            progress.skin=res[0].url;
+            progress.value=0.6;
+            Laya.stage.addChild(progress);
+        })
     }
 }
