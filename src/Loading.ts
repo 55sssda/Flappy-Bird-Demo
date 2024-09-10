bfcfa54563596670b7e2434478783fd67c9e5c12
cza <840270646@QQ.COM>
2024-09-10 23:26:31 +0800

    加背景组件

--- src/Loading.ts
+++ src/Loading.ts
@@ -1,10 +1,13 @@
 const { regClass } = Laya;
 import { LoadingBase } from "./Loading.generated";
+import background from "./view/background/background";
+import progress from "./view/progress/progress";
 @regClass()
 export class Loading extends LoadingBase {
     onAwake(): void {
         Laya.loader.load(["resources/progress.png","resources/progress$bar.png","resources/bg_day.png","resources/bg_night.png"]).then((res)=>{
-            
+            const bg=new background(res[2],res[3]);
+            const pg=new progress(res[0],res[1]);
         })
     }
 }
