9bed583177dd658b0edeec031d974781814879b3
cza <840270646@QQ.COM>
2024-09-12 23:16:53 +0800

    1

--- src/Loading.ts
+++ src/Loading.ts
@@ -8,6 +8,7 @@ export class Loading extends LoadingBase {
         Laya.loader.load(["resources/progress.png","resources/progress$bar.png","resources/bg_day.png","resources/bg_night.png"]).then((res)=>{
             const bg=new background(res[2],res[3]);
             const pg=new progress(res[0],res[1]);
+            this.progress.value=0.5;
         })
     }
 }
--- src/view/progress/progress.ts
+++ src/view/progress/progress.ts
@@ -1,16 +1,16 @@
 class progress{
     constructor(...url:Array<any>){
-        console.log(url);
         const bar=new Laya.ProgressBar();
         bar.skin=url[0].url;
         bar.anchorX=0.5;
         bar.anchorY=0.5;
         bar.x=Laya.stage.width/2;
         bar.y=Laya.stage.height/1.2;
-        bar.width=500;
-        bar.height=40;
-        bar.value=0.3;
+        bar.width=Laya.stage.width*0.8;
+        bar.height=Laya.stage.width*0.04;
+        bar.value=0;
         Laya.stage.addChild(bar);
+        Laya.stage.getChildByName('root').getChildByName('Scene2D').progress=bar;
     }
 }
 export default progress;
